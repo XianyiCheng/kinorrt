b20c6fb7ba2cee3a511a6fa7a3468ae6ba3379ba
xianyi <chengxy0418@gmail.com>
2020-05-07 16:12:06 -0400

    test files

---
+++ tests/test_kinorrt.py
@@ -0,0 +1,532 @@
+
+from time import time
+
+import glm
+from itbl import Ray, Shader
+from itbl.accelerators import SDF, BVHAccel
+from itbl.cameras import TrackballCamera
+
+from itbl.shapes import Box
+from itbl.util import get_color, get_data
+from itbl.viewer import Application, Viewer
+from itbl.viewer.backend import *
+from wilson import *
+import itbl._itbl as _itbl
+import time
+
+from kinorrt.search_space import SearchSpace
+from kinorrt.rrt import RRTKinodynamic, RRTKino_w_modes
+from kinorrt.mechanics.contact_kinematics import *
+from contact_modes.modes_3d import enum_sliding_sticking_3d_proj
+
+OBJECT_SHAPE = [1.75, 1, 1.5, 0.75]
+HALLWAY_W = 2.5
+BLOCK_H = 7
+BLOCK_W = 5
+np.seterr(divide='ignore')
+np.set_printoptions(suppress=True, precision=4, linewidth=210)
+
+
+def print_opengl_error():
+    err = glGetError()
+    if (err != GL_NO_ERROR):
+        print('GLError: ', gluErrorString(err))
+
+def config2trans(q):
+    q = q.flatten()
+    a = q[2]
+    g = np.identity(3)
+    R = np.array([[np.cos(a),-np.sin(a)],[np.sin(a), np.cos(a)]])
+    g[0:2,0:2] = R
+    g[0:2,-1] = q[0:2]
+    return g
+
+
+
+class iTM2d(Application):
+    def __init__(self, object_shape, example='sofa'):
+        # Initialize scene.
+        super(iTM2d, self).__init__(None)
+
+        self.mesh = Box(1.0, 0.5, 0.2)
+        self.light_box = Box(0.2, 0.2, 0.2)
+        self.example = example
+        self.object_shape = object_shape
+
+    def init(self):
+        super(iTM2d, self).init()
+
+        # Basic lighting shader.
+        vertex_source = os.path.join(get_data(), 'shader', 'basic_lighting.vs')
+        fragment_source = os.path.join(get_data(), 'shader', 'basic_lighting.fs')
+        self.basic_lighting_shader = Shader(vertex_source, fragment_source)
+
+        # Lamp shader.
+        vertex_source = os.path.join(get_data(), 'shader', 'flat.vs')
+        fragment_source = os.path.join(get_data(), 'shader', 'flat.fs')
+        self.lamp_shader = Shader(vertex_source, fragment_source)
+
+        # Normal shader.
+        vertex_source = os.path.join(get_data(), 'shader', 'normals.vs')
+        fragment_source = os.path.join(get_data(), 'shader', 'normals.fs')
+        geometry_source = os.path.join(get_data(), 'shader', 'normals.gs')
+        self.normal_shader = Shader(vertex_source, fragment_source, geometry_source)
+
+        # Trackball camera.
+        self.camera = TrackballCamera(radius=50)
+
+        # Toggle variables.
+        self.draw_mesh = True
+        self.draw_wireframe = True
+        self.draw_normals = False
+
+    def init2(self):
+        # C++ OpenGL.
+        _itbl.loadOpenGL()
+
+        # 2D shader.
+        vertex_source = os.path.join(get_data(), 'shader', '2d.vs')
+        fragment_source = os.path.join(get_data(), 'shader', '2d.fs')
+        self.flat_shader = Shader(vertex_source, fragment_source)
+
+        # Object
+
+        self.env_contacts = None
+        self.manip_contacts = None
+        self.env_contacts = None
+        self.manifold = None
+        self.v_m = None
+        self.counter = 0
+        self.target = _itbl.Rectangle(self.object_shape[0] * 2, self.object_shape[1] * 2, 2, 0.0)
+        if self.example == 'sofa':
+            self.collision_manager = create_hallway(HALLWAY_W, BLOCK_W, BLOCK_H, self.object_shape[
+                0] * 2.5 + BLOCK_W * 0.5)  # uniform_sample_maze((4,4), 3, 1.25)
+        elif self.example == 'maze':
+            self.collision_manager = uniform_sample_maze((3, 3), 3, 1.25)
+        elif self.example == 'corner':
+            self.collision_manager = corner()
+        elif self.example == 'wall':
+            self.collision_manager = wall()
+        elif self.example == 'table':
+            self.collision_manager = corner()
+        elif self.example == 'obstacle_course':
+            self.collision_manager = obstacle_course()
+        else:
+            print('Cannot find collision manager!')
+            raise
+
+        self.all_configs_on = False
+        self.step_on = False
+        self.path_on = False
+
+        self.manip_p = None
+        self.next_manip_p = None
+
+    def draw_manifold(self):
+        if self.manifold is None:
+            return
+
+        glPointSize(5)
+        manifold = self.manifold
+        for i in range(len(manifold.depths)):
+            glBegin(GL_POINTS)
+            cp = manifold.contact_points[i]
+            glVertex3f(cp[0], cp[1], 1)
+            glEnd()
+            glBegin(GL_LINES)
+            d = manifold.depths[i]
+            n = manifold.normals[i]
+            cq = cp - d * n
+            glVertex3f(cp[0], cp[1], 1)
+            glVertex3f(cq[0], cq[1], 1)
+            glEnd()
+
+    def draw_ground(self):
+        glBegin(GL_LINES)
+        # ground line
+        glVertex3f(-10, 0, -1)
+        glVertex3f(10, 0, -1)
+        # hashes
+        for x in np.arange(-10, 10, 0.1):
+            glVertex3f(x, 0, -1)
+            glVertex3f(x - 0.1, -0.1, -1)
+        glEnd()
+
+    def draw_grid(self, size, step):
+        glBegin(GL_LINES)
+
+        glColor3f(0.3, 0.3, 0.3)
+        for i in np.arange(step, size, step):
+            glVertex3f(-size, i, 0)  # lines parallel to X-axis
+            glVertex3f(size, i, 0)
+            glVertex3f(-size, -i, 0)  # lines parallel to X-axis
+            glVertex3f(size, -i, 0)
+
+            glVertex3f(i, -size, 0)  # lines parallel to Z-axis
+            glVertex3f(i, size, 0)
+            glVertex3f(-i, -size, 0)  # lines parallel to Z-axis
+            glVertex3f(-i, size, 0)
+
+        # x-axis
+        glColor3f(0.5, 0, 0)
+        glVertex3f(-size, 0, 0)
+        glVertex3f(size, 0, 0)
+
+        # z-axis
+        glColor3f(0, 0, 0.5)
+        glVertex3f(0, -size, 0)
+        glVertex3f(0, size, 0)
+
+        glEnd()
+
+    def render(self):
+        glClearColor(0.2, 0.3, 0.3, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_MULTISAMPLE)
+        glEnable(GL_BLEND)
+        # glEnable(GL_CULL_FACE)
+
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
+        self.basic_lighting_shader.use()
+        print_opengl_error()
+
+        model = glm.mat4(1.0)
+        self.basic_lighting_shader.set_mat4('model', np.asarray(model))
+
+        view = self.camera.get_view()
+        self.basic_lighting_shader.set_mat4('view', np.asarray(view))
+
+        projection = glm.perspective(glm.radians(45.0), 1200. / 900, 0.1, 100.0)
+        self.basic_lighting_shader.set_mat4('projection', np.asarray(projection))
+
+        # colors
+        # self.basic_lighting_shader.set_vec3('objectColor', np.array([1.0, 0.5, 0.31], 'f'))
+        self.basic_lighting_shader.set_vec3('lightColor', np.array([1.0, 1.0, 1.0], 'f'))
+
+        # light
+        lightPos = glm.vec3([1.00, 1.75, 10.0])
+        self.basic_lighting_shader.set_vec3('lightPos', np.asarray(lightPos))
+
+        # camera
+        cameraPos = glm.vec3(glm.column(glm.inverse(view), 3))
+        self.basic_lighting_shader.set_vec3('viewPos', np.asarray(cameraPos))
+
+        # Draw object.
+        if self.draw_mesh:
+            # Draw obstacles.
+            self.basic_lighting_shader.set_vec3('objectColor', get_color('gray'))
+            self.collision_manager.draw(self.basic_lighting_shader.id, True, True)
+
+            # Draw object.
+            self.basic_lighting_shader.set_vec3('objectColor', get_color('clay'))
+            self.target.draw3d(self.basic_lighting_shader.id)
+
+        # Draw normals.
+        self.normal_shader.use()
+        self.normal_shader.set_mat4('model', np.asarray(model))
+        self.normal_shader.set_mat4('view', np.asarray(view))
+        self.normal_shader.set_mat4('projection', np.asarray(projection))
+
+        if self.draw_normals:
+            self.mesh.draw(self.normal_shader)
+
+        # Draw edges and light.
+        self.lamp_shader.use()
+        self.lamp_shader.set_mat4('model', np.asarray(model))
+        self.lamp_shader.set_mat4('view', np.asarray(view))
+        self.lamp_shader.set_mat4('projection', np.asarray(projection))
+        self.lamp_shader.set_vec3('objectColor', np.ones((3, 1), 'float32'))
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        if self.draw_wireframe:
+            # Draw object.
+            self.target.draw3d(self.lamp_shader.id)
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        light_model = glm.mat4(1.0)
+        light_model = glm.translate(light_model, lightPos)
+        self.lamp_shader.set_mat4('model', np.asarray(light_model))
+        # self.light_box.draw(self.lamp_shader)
+
+        self.lamp_shader.set_mat4('model', np.asarray(model))
+        self.lamp_shader.set_vec3('objectColor', get_color('teal'))
+
+        model = glm.mat4(1.0)
+        self.lamp_shader.set_vec3('objectColor', np.ones((3, 1), 'float32'))
+        self.lamp_shader.set_mat4('model', np.asarray(model))
+        # self.draw_grid(5, 0.25)
+
+    def render2(self):
+        glClearColor(0.2, 0.3, 0.3, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_MULTISAMPLE)
+        # glEnable(GL_BLEND)
+        # glEnable(GL_CULL_FACE)
+        # glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        # glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        self.flat_shader.use()
+        model = glm.mat4(1.0)
+        self.flat_shader.set_mat4('model', np.asarray(model))
+        view = glm.mat4(1.0)
+        self.flat_shader.set_mat4('view', np.asarray(view))
+        aspect_ratio = 800. / 600.
+        d = 10
+        ortho = glm.ortho(-d * aspect_ratio, d * aspect_ratio, -d, d, -100.0, 100.0)
+        # ortho = glm.ortho(-2*aspect_ratio, 2*aspect_ratio, -2, 2, -100.0, 100.0)
+        self.flat_shader.set_mat4('projection', np.asarray(ortho))
+        self.flat_shader.set_vec3('offset', np.zeros((3, 1), 'float32'))
+        self.flat_shader.set_float('scale', 1.0)
+        self.flat_shader.set_vec3('objectColor', np.ones((3, 1), 'float32'))
+        # self.draw_grid(5, 0.25)
+
+        # Draw obstacles.
+        self.flat_shader.set_vec3('objectColor', get_color('gray'))
+        self.collision_manager.draw(self.flat_shader.id, True, False)
+
+        if self.step_on:
+            # Draw object.
+            new_m = point_manipulator()
+            if self.counter >= len(self.path):
+                self.counter = 0
+
+            self.config = self.path[self.counter]
+            self.manip_p = self.mnp_path[self.counter]
+
+            if self.manip_p is not None:
+                for mnp in self.manip_p:
+                    p = mnp.p
+                    p = p[0:2]
+                    new_m.update_config(np.array(p),self.config)
+                    self.flat_shader.set_vec3('objectColor', get_color('red'))
+                    new_m.obj.draw2d(self.flat_shader.id, True)
+
+            self.flat_shader.set_vec3('objectColor', get_color('clay'))
+            T2 = config2trans(np.array(self.config))
+            T3 = np.identity(4)
+            T3[0:2, 3] = T2[0:2, 2]
+            T3[0:2, 0:2] = T2[0:2, 0:2]
+            self.target.transform()[:, :] = T3
+            self.target.draw2d(self.flat_shader.id, True)
+
+            # print(self.counter, len(self.path))
+            time.sleep(0.07)
+            self.counter += 1
+
+        if self.path_on:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+            for i in range(len(self.path)):
+                self.flat_shader.set_vec3('objectColor', get_color('clay'))
+                target_config = self.path[i]
+                T2 = config2trans(np.array(target_config))
+                T3 = np.identity(4)
+                T3[0:2, 3] = T2[0:2, 2]
+                T3[0:2, 0:2] = T2[0:2, 0:2]
+                self.target.transform()[:, :] = T3
+                self.target.draw2d(self.flat_shader.id, True)
+
+        if self.all_configs_on:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+            # show all nodes
+            for node in self.nodes:
+                self.flat_shader.set_vec3('objectColor', get_color('clay'))
+                target_config = np.array(node)
+                T2 = config2trans(target_config)
+                T3 = np.identity(4)
+                T3[0:2, 3] = T2[0:2, 2]
+                T3[0:2, 0:2] = T2[0:2, 0:2]
+                self.target.transform()[:, :] = T3
+                self.target.draw2d(self.flat_shader.id, True)
+
+    def on_key_press2(self, key, scancode, action, mods):
+        if key == glfw.KEY_C and action == glfw.PRESS:
+            self.step_on = False
+            self.path_on = False
+            self.all_configs_on = False
+        if key == glfw.KEY_T and action == glfw.PRESS:
+            self.step_on = True
+
+        if key == glfw.KEY_A and action == glfw.PRESS:
+            self.all_configs_on = True
+        if key == glfw.KEY_P and action == glfw.PRESS:
+            self.path_on = True
+
+    def on_key_press(self, key, scancode, action, mods):
+        pass
+
+    # def on_mouse_press(self, x, y, button, modifiers):
+    #     pass
+
+    # def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+    #     pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        x = 2.0 * (x / 800.0) - 1.0
+        y = 2.0 * (y / 600.0) - 1.0
+        if button == 1:  # left click
+            self.camera.mouse_roll(x, y, False)
+        if button == 4:  # right click
+            self.camera.mouse_zoom(x, y, False)
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        x = 2.0 * (x / 800.0) - 1.0
+        y = 2.0 * (y / 600.0) - 1.0
+        if buttons == 1:  # left click
+            self.camera.mouse_roll(x, y)
+        if buttons == 4:  # right click
+            self.camera.mouse_zoom(x, y)
+
+    def get_path(self, path, mnp_path):
+        self.path = path
+        self.mnp_path = mnp_path
+
+    def get_nodes(self, nodes):
+        self.nodes = nodes
+    def get_tree(self, tree):
+        self.tree = tree
+
+
+
+
+def DrawContactInObjectFrame(contact, obj_shader):
+    if contact is None:
+        return
+    if len(contact) == 0:
+        return
+    for i in range(len(contact)):
+        glPointSize(5)
+        p = contact[i][0]
+        n = contact[i][1]
+        glBegin(GL_POINTS)
+        glVertex3f(p[0], p[1], 1)
+        glEnd()
+        glBegin(GL_LINES)
+        q = p + 0.2 * n
+        glVertex3f(p[0], p[1], 1)
+        glVertex3f(q[0], q[1], 1)
+        glEnd()
+
+
+def test_kinorrt_cases(keyword = 'sofa'):
+
+    viewer = Viewer()
+    _itbl.loadOpenGL()
+    manipulator = point_manipulator()
+    mnp_fn_max = None
+    step_length = 2
+
+    if keyword == 'sofa':
+        neighbor_r = 5
+        object_shape = [1.75, 1, 1.5, 0.75]
+        X_dimensions = np.array([(0, 14.5), (0, 14.5), (-np.pi, np.pi)])  # dimensions of Search Space
+        x_init = (0, OBJECT_SHAPE[0] / 2, np.pi / 2)  # starting location
+        x_goal = (OBJECT_SHAPE[0] * 5 + BLOCK_W, HALLWAY_W + BLOCK_H * 2 - OBJECT_SHAPE[1] - 1, np.pi / 2)
+        world_key = 'planar'
+        dist_weight = 50
+        dist_cost = 0.5
+        manipulator = doublepoint_manipulator()
+    elif keyword == 'corner':
+        neighbor_r = 5
+        object_shape = [0.5, 0.5, 0.2, 0.2]
+        X_dimensions = np.array([(0, 4), (0, 4), (-np.pi, np.pi)])
+        x_init = (3, 0.5, 0)  # starting location
+        x_goal = (0.707, 0.707, np.pi / 4)
+        # x_init = (2, 0.5, 0)
+        # x_goal = (0.5, 0.5, -np.pi / 2)
+        world_key = 'vert'
+        dist_weight = 1
+        dist_cost = 0.4
+        manipulator = doublepoint_manipulator()
+        mnp_fn_max = 6
+        step_length = 2
+    elif keyword == 'wall':
+        neighbor_r = 10
+        object_shape = [0.5,0.5,0.2,0.2]
+        X_dimensions = np.array([(-8, 8), (0, 7 + object_shape[1]*4), (-np.pi, np.pi)])
+        x_init = (4.5, 0.5, 0)
+        x_goal = (-3, 7.5, np.pi/2)
+        world_key = 'vert'
+        dist_weight = 1
+        dist_cost = 0.2
+        manipulator = point_manipulator()
+        step_length = 10
+        mnp_fn_max = 50
+        goal_kch = [1,1,0]
+    elif keyword == 'table':
+        keyword = 'corner'
+        neighbor_r = 5
+        object_shape = [0.5, 0.25, 0.2, 0.2]
+        X_dimensions = np.array([(0, 4), (0, 4), (-2*np.pi, 2*np.pi)])
+        # x_init = (3, 0.5, 0)  # starting location
+        # x_goal = (0.707, 0.707, np.pi / 4)
+        x_init = (2, 0.25, 0)
+        x_goal = (2, 0.25, -np.pi)
+        world_key = 'vert'
+        dist_weight = 100
+        dist_cost = 1
+        manipulator = doublepoint_manipulator()
+        mnp_fn_max = 6
+        step_length = 3.14
+        goal_kch = [0,0,1]
+    elif keyword == 'obstacle_course':
+        neighbor_r = 5
+        object_shape = [0.5, 0.5, 0.2, 0.2]
+        X_dimensions = np.array([(-2.5,3), (0, 4), (-2*np.pi, 2*np.pi)])
+        # x_init = (3, 0.5, 0)  # starting location
+        # x_goal = (0.707, 0.707, np.pi / 4)
+        x_init = (-2.5, 1.5, 0)
+        x_goal = (2.5, 1.5, 0)
+        world_key = 'vert'
+        dist_weight = 0.1
+        dist_cost = 1
+        manipulator = doublepoint_manipulator()
+        mnp_fn_max = 6
+        goal_kch = [1,1,0]
+    else:
+        print('Wrong case keyword!')
+        raise
+
+
+    app = iTM2d(object_shape, example=keyword)
+    viewer.set_renderer(app)
+    viewer.init()
+
+    X = SearchSpace(X_dimensions)
+    max_samples = 50  # max number of samples to take before timing out
+
+    the_object = part(app.target, object_shape)
+
+    kino_tree = RRTKino_w_modes(X, x_init, x_goal, environment(app.collision_manager), the_object, manipulator, max_samples, neighbor_r, world_key)
+    kino_tree.mnp_fn_max = mnp_fn_max
+    kino_tree.dist_weight = dist_weight
+    kino_tree.cost_weight[0] = dist_cost
+    kino_tree.step_length = step_length
+    kino_tree.goal_kCh = goal_kch
+
+    kino_tree.initialize_stability_margin_solver()
+    t_start = time.time()
+    path, mnp_path = kino_tree.search_bias()
+    t_end = time.time()
+    print('time:', t_end - t_start)
+
+    # t_start = time.time()
+    # path = kino_tree.search()
+    # t_end = time.time()
+
+    print('time:', t_end - t_start)
+
+    app.get_path(path, mnp_path)
+    app.get_nodes(kino_tree.trees[0].nodes)
+    app.get_tree(kino_tree)
+    viewer.start()
+
+    return
+
+np.random.seed(10)
+ti = test_kinorrt_cases('obstacle_course')
